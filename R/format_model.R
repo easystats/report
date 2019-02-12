352cf558834bd868a36beeb03f359a2f202fd007
Dominique Makowski <dom.mak19@gmail.com>
2019-02-12 10:43:28 +0800

    why does it fail in local?

--- R/format_model.R
+++ R/format_model.R
@@ -4,26 +4,38 @@
 #'
 #' @examples
 #' model <- lm(Sepal.Length ~ Species, data=iris)
+#' format_model(model)
+#'
+#' @importFrom stringr str_detect
+#' @importFrom insight model_info
 #' @export
 format_model <- function(model) {
   info <- insight::model_info(model)
 
   type <- ""
 
-  if(info$is_bayesian0) type <- paste0(type, "Bayesian ")
+  if(info$is_bayesian){
+    type <- paste0(type, "Bayesian ")
+  }
+
+  if(info$is_zeroinf){
+    type <- paste0(type, "zero-inflated ")
+  }
+  # TODO: hurdle?
 
   if(info$is_logit){
     type <- paste0(type, "logistic ")
-    # TODO: is_probit
+  } else if(info$is_probit){
+    type <- paste0(type, "probit ")
   } else if(info$is_linear){
     type <- paste0(type, "linear ")
   } else{
     type <- paste0(type, "general linear ")
   }
 
-
-  if(info$is_zeroinf) type <- paste0(type, "zero-inflated ")
-  # if(info$is_mixed) type <- paste0(type, "mixed ")
+  if(info$is_mixed){
+    type <- paste0(type, "mixed ")
+  }
 
   type <- paste0(type, "model")
 
