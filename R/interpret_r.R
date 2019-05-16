e1ba9736978a629129c7eb4d6332c80a0c958ef4
Dominique Makowski <dom.mak19@gmail.com>
2019-05-16 09:53:35 +0800

    revise default effect size interpretation for r and d

--- R/interpret_r.R
+++ R/interpret_r.R
@@ -1,7 +1,7 @@
 #' Correlation Coefficient Interpretation
 #'
 #' @param r Value or vector of correlation coefficient.
-#' @param rules Can be "gignac2016" (default), cohen1988", "evans1996" or custom set of rules.
+#' @param rules Can be "funder2019" (default), "gignac2016", cohen1988", "evans1996" or custom set of rules.
 #'
 #'
 #'
@@ -12,16 +12,25 @@
 #' @seealso Page 88 of APA's 6th Edition.
 #' @references
 #' \itemize{
+#'   \item Funder, D. C., & Ozer, D. J. (2019). Evaluating effect size in psychological research: sense and nonsense. Advances in Methods and Practices in Psychological Science.
 #'   \item Gignac, G. E., & Szodorai, E. T. (2016). Effect size guidelines for individual differences researchers. Personality and individual differences, 102, 74-78.
 #'   \item Cohen, J. (1988). Statistical power analysis for the behavioural sciences.
 #'   \item Evans, J. D. (1996). Straightforward statistics for the behavioral sciences. Thomson Brooks/Cole Publishing Co.
 #' }
 #' @export
-interpret_r <- function(r, rules = "gignac2016") {
+interpret_r <- function(r, rules = "funder2019") {
   if (is.rules(rules)) {
     return(interpret(abs(r), rules))
   } else {
-    if (rules == "gignac2016") {
+    if (rules == "funder2019") {
+      text <- interpret(
+        abs(r),
+        rules(
+          c(0.05, 0.1, 0.2, 0.3, 0.4),
+          c("very small", "small", "medium", "large", "very large")
+        )
+      )
+    } else if (rules == "gignac2016") {
       text <- interpret(
         abs(r),
         rules(
