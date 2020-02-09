70223d1586e6788e444c1ba06379edc24033eec0
DominiqueMakowski <dom.mak19@gmail.com>
2020-02-09 08:35:56 +0800

    report.bayesfactor

--- R/report.bayestestR.R
+++ R/report.bayestestR.R
@@ -8,7 +8,6 @@
 #'
 #'
 #' @examples
-#' \donttest{
 #' library(bayestestR)
 #' library(report)
 #'
@@ -17,13 +16,19 @@
 #' mo2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
 #' mo3 <- lm(Sepal.Length ~ Species * Petal.Length, data = iris)
 #'
+#' # Bayes factor - models
 #' BFmodels <- bayesfactor_models(mo1, mo2, mo3, denominator = mo0)
 #'
-#' bf_report <- report(BFmodels, digits = 3)
-#' to_table(bf_report)
-#' to_text(bf_report, full = FALSE)
+#' r <- report(BFmodels)
+#' r
+#' table_short(r)
 #'
-#' }
+#' # Bayes factor - inclusion
+#' inc_bf <- bayesfactor_inclusion(BFmodels, prior_odds = c(1,2,3), match_models = TRUE)
+#'
+#' r <- report(inc_bf)
+#' r
+#' table_short(r)
 #' @seealso report
 #' @importFrom stats setNames
 #' @export
@@ -126,132 +131,103 @@ report.bayesfactor_models <- function(model, interpretation = "jeffreys1961", ..
 
 
 
-#' Inclusion Bayes factor Report
-#'
-#' Create a report of inclusion Bayes factors.
-#'
-#' @param model Object of class \code{bayesfactor_inclusion}.
-#' @param ... Arguments passed to or from other methods.
-#' @inheritParams interpret_bf
-#'
-#'
-#' @examples
-#' \donttest{
-#' library(bayestestR)
-#' library(report)
-#'
-#' mo0 <- lm(Sepal.Length ~ 1, data = iris)
-#' mo1 <- lm(Sepal.Length ~ Species, data = iris)
-#' mo2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
-#' mo3 <- lm(Sepal.Length ~ Species * Petal.Length, data = iris)
-#'
-#' BFmodels <- bayesfactor_models(mo1, mo2, mo3, denominator = mo0)
-#' inc_bf <- bayesfactor_inclusion(BFmodels, prior_odds = c(1,2,3), match_models = TRUE)
-#'
-#' bf_report <- report(inc_bf, digits = 3)
-#' to_table(bf_report)
-#' to_text(bf_report, full = FALSE)
-#'
-#' }
-#' @seealso report
+#' @rdname report.bayesfactor_models
 #' @importFrom stats setNames
 #' @export
-# report.bayesfactor_inclusion <- function(model, rules = "jeffreys1961", ...){
-#   matched <- attr(model,"matched")
-#   priorOdds <- attr(model,"priorOdds")
-#
-#   #### text ####
-#   bf_results <- data.frame(Term = rownames(model), stringsAsFactors = FALSE)
-#   bf_results$evidence <- interpret_bf(model$BF,rules = rules, include_value = TRUE)
-#   bf_results$postprob <- paste0(round(model$p_posterior*100,...),"%")
-#
-#   bf_text <- paste0(
-#     "Bayesian model averaging (BMA) was used to obtain the average evidence ",
-#     "for each predictor. We found ",
-#     paste0(
-#       paste0(bf_results$evidence," including ",bf_results$Term),
-#       collapse = "; "
-#     ),"."
-#   )
-#
-#   #### text full ####
-#   bf_explain <- paste0(
-#     "Bayesian model averaging (BMA) was used to obtain the average evidence ",
-#     "for each predictor. Since each model has a prior probability",
-#     # custom priors?
-#     switch(!is.null(priorOdds) + 1,NULL,paste0(" (here we used subjective prior odds of ",
-#                                                paste0(priorOdds, collapse = ", "), ")")),
-#     ", it is possible to sum the prior probability of all models that include ",
-#     "a predictor of interest (the prior inclusion probability), and of all ",
-#     "models that do not include that predictor (the prior exclusion probability). ",
-#     "After the data are observed, we can similarly consider the sums of the ",
-#     "posterior models' probabilities to obtain the posterior inclusion ",
-#     "probability and the posterior exclusion probability. The change from ",
-#     "prior to posterior inclusion odds is the Inclusion Bayes factor. ",
-#     # matched models?
-#     switch(!matched + 1,NULL,
-#            paste0(
-#              "For each predictor, averaging was done only across models that ",
-#              "did not include any interactions with that predictor; ",
-#              "additionally, for each interaction predictor, averaging was done ",
-#              "only across models that contained the main effect from which the ",
-#              "interaction predictor was comprised. This was done to prevent ",
-#              "Inclusion Bayes factors from being contaminated with non-relevant ",
-#              "evidence (see Mathot, 2017). "
-#            ))
-#   )
-#   bf_text_full <- paste0(
-#     bf_explain,
-#     paste0(
-#       "We found ",
-#       paste0(
-#         paste0(bf_results$evidence," including ",bf_results$Term,
-#                ", with models including ",bf_results$Term,
-#                " having an overall posterior probability of ",bf_results$postprob),
-#         collapse = "; "
-#       ),"."
-#     )
-#   )
-#
-#   #### table ####
-#   bf_table <- as.data.frame(model)
-#   colnames(bf_table) <- c("Pr(prior)", "Pr(posterior)", "Inclusion BF")
-#   bf_table <- cbind(Terms = rownames(bf_table),bf_table)
-#   rownames(bf_table) <- NULL
-#   bf_table$`Inclusion BF` <- bayestestR:::.format_big_small(bf_table$`Inclusion BF`,...)
-#   bf_table[,2:3] <- insight::format_value(bf_table[,2:3], ...)
-#
-#   # make table footer
-#   table_footer <- matrix(rep("",12),nrow = 3)
-#   table_footer[2:3,1] <- c(
-#     ifelse(matched,
-#            "Across matched models only,",
-#            "Across all models,"),
-#     ifelse(is.null(priorOdds),
-#            "assuming unifor prior odds.",
-#            paste0("with custom prior odds of [", paste0(priorOdds, collapse = ", "), "]."))
-#   )
-#   # pad with empty cells:
-#   colnames(table_footer) <- colnames(bf_table)
-#
-#   bf_table <- rbind(bf_table,table_footer)
-#
-#
-#   #### table full ####
-#   bf_table_full <- bf_table
-#
-#
-#   #### values ####
-#   bf_values <- as.list(setNames(model$BF,rownames(model)))
-#
-#   out <- list(
-#     text = bf_text,
-#     text_full = bf_text_full,
-#     table = bf_table,
-#     table_full = bf_table_full,
-#     values = bf_values
-#   )
-#
-#   as.report(out, rules = rules, priorOdds = priorOdds, matched = matched, ...)
-# }
+report.bayesfactor_inclusion <- function(model, interpretation = "jeffreys1961", ...){
+  matched <- attr(model,"matched")
+  priorOdds <- attr(model,"priorOdds")
+
+  #### text ####
+  bf_results <- data.frame(Term = rownames(model), stringsAsFactors = FALSE)
+  bf_results$evidence <- effectsize::interpret_bf(model$BF,rules = interpretation, include_value = TRUE)
+  bf_results$postprob <- paste0(round(model$p_posterior*100,...),"%")
+
+  bf_text <- paste0(
+    "Bayesian model averaging (BMA) was used to obtain the average evidence ",
+    "for each predictor. We found ",
+    paste0(
+      paste0(bf_results$evidence," including ",bf_results$Term),
+      collapse = "; "
+    ),"."
+  )
+
+  #### text full ####
+  bf_explain <- paste0(
+    "Bayesian model averaging (BMA) was used to obtain the average evidence ",
+    "for each predictor. Since each model has a prior probability",
+    # custom priors?
+    switch(!is.null(priorOdds) + 1,NULL,paste0(" (here we used subjective prior odds of ",
+                                               paste0(priorOdds, collapse = ", "), ")")),
+    ", it is possible to sum the prior probability of all models that include ",
+    "a predictor of interest (the prior inclusion probability), and of all ",
+    "models that do not include that predictor (the prior exclusion probability). ",
+    "After the data are observed, we can similarly consider the sums of the ",
+    "posterior models' probabilities to obtain the posterior inclusion ",
+    "probability and the posterior exclusion probability. The change from ",
+    "prior to posterior inclusion odds is the Inclusion Bayes factor. ",
+    # matched models?
+    switch(!matched + 1,NULL,
+           paste0(
+             "For each predictor, averaging was done only across models that ",
+             "did not include any interactions with that predictor; ",
+             "additionally, for each interaction predictor, averaging was done ",
+             "only across models that contained the main effect from which the ",
+             "interaction predictor was comprised. This was done to prevent ",
+             "Inclusion Bayes factors from being contaminated with non-relevant ",
+             "evidence (see Mathot, 2017). "
+           ))
+  )
+  bf_text_full <- paste0(
+    bf_explain,
+    paste0(
+      "We found ",
+      paste0(
+        paste0(bf_results$evidence," including ",bf_results$Term,
+               ", with models including ",bf_results$Term,
+               " having an overall posterior probability of ",bf_results$postprob),
+        collapse = "; "
+      ),"."
+    )
+  )
+
+  #### table ####
+  bf_table <- as.data.frame(model)
+  colnames(bf_table) <- c("Pr(prior)", "Pr(posterior)", "Inclusion BF")
+  bf_table <- cbind(Terms = rownames(bf_table),bf_table)
+  rownames(bf_table) <- NULL
+  bf_table$`Inclusion BF` <- bayestestR:::.format_big_small(bf_table$`Inclusion BF`,...)
+  bf_table[,2:3] <- insight::format_value(bf_table[,2:3], ...)
+
+  # make table footer
+  table_footer <- matrix(rep("",12),nrow = 3)
+  table_footer[2:3,1] <- c(
+    ifelse(matched,
+           "Across matched models only,",
+           "Across all models,"),
+    ifelse(is.null(priorOdds),
+           "assuming unifor prior odds.",
+           paste0("with custom prior odds of [", paste0(priorOdds, collapse = ", "), "]."))
+  )
+  # pad with empty cells:
+  colnames(table_footer) <- colnames(bf_table)
+
+  bf_table <- rbind(bf_table,table_footer)
+
+
+  #### table full ####
+  bf_table_full <- bf_table
+
+
+  #### out ####
+  tables <- as.model_table(bf_table, bf_table_full)
+  texts <- as.model_text(bf_text, bf_text_full)
+
+  out <- list(
+    texts = texts,
+    tables = tables
+  )
+
+  as.report(out, interpretation = interpretation, priorOdds = priorOdds, matched = matched, ...)
+}
 
---
+++ tests/testthat/test-report.bayesfactors.R
@@ -0,0 +1,20 @@
+context("report.bayesfactor*")
+
+library(bayestestR)
+
+mod0 <- lm(Sepal.Length ~ 1, data = iris)
+mod1 <- lm(Sepal.Length ~ Species, data = iris)
+mod2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
+mod3 <- lm(Sepal.Length ~ Species * Petal.Length, data = iris)
+
+BFmodels <- bayesfactor_models(mod1, mod2, mod3, denominator = mod0)
+
+testthat::test_that("models", {
+  testthat::expect_equal(nchar(text_short(report(BFmodels))), 186)
+})
+
+inc_bf <- bayesfactor_inclusion(BFmodels, prior_odds = c(1,2,3), match_models = TRUE)
+
+testthat::test_that("inclusion", {
+  testthat::expect_equal(nchar(text_short(report(inc_bf))), 308)
+})
