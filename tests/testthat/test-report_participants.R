f5e50b0d0a8e6e669fe3ef1a708e629885e55d2d
Daniel <mail@danielluedecke.de>
2020-02-21 15:18:09 +0100

    tests

--- tests/testthat/test-report_participants.R
+++ tests/testthat/test-report_participants.R
@@ -8,6 +8,13 @@ test_that("report_participants", {
     "Participant" = c("S1", "S1", "s2", "s2", "s3", "s3")
   )
 
-  testthat::expect_equal(nchar(report_participants(data, age = "Age", sex = "Sex", participant = "Participant")), 155)
-  testthat::expect_equal(nchar(report_participants(data, participant = "Participant", spell_n = TRUE)), 159)
+  testthat::expect_equal(
+    report_participants(data, age = "Age", sex = "Sex", participant = "Participant"),
+    "3 participants (Mean age = 28.00, SD = 23.58, range = [8, 54]; 66.67% females)"
+  )
+  testthat::expect_equal(nchar(report_participants(data, participant = "Participant", spell_n = TRUE)), 82)
+  testthat::expect_equal(
+    report_participants(data, participant = "Participant", spell_n = TRUE),
+    "Three participants (Mean age = 28.00, SD = 23.58, range = [8, 54]; 66.67% females)"
+  )
 })
