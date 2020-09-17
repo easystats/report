c67a2dfe339951b07350fe997969558f0bbb47e8
Daniel <mail@danielluedecke.de>
2020-09-17 12:28:02 +0200

    fix tests

--- tests/testthat/test-format_model.R
+++ tests/testthat/test-format_model.R
@@ -1,18 +1,19 @@
-if (require("testthat") && require("insight") && require("httr"))
-test_that("format_model", {
-  testthat::expect_equal(format_model(insight::download_model("lm_1")), "linear model")
-  testthat::expect_equal(format_model(insight::download_model("glm_1")), "logistic model")
-  testthat::expect_equal(format_model(insight::download_model("glm_2")), "logistic model")
-  testthat::expect_equal(format_model(insight::download_model("glm_3")), "logistic model")
-  testthat::expect_equal(format_model(insight::download_model("glm_4")), "probit model")
+if (require("testthat") && require("insight") && require("httr")) {
+  test_that("format_model", {
+    testthat::expect_equal(format_model(insight::download_model("lm_1")), "linear model")
+    testthat::expect_equal(format_model(insight::download_model("glm_1")), "logistic model")
+    testthat::expect_equal(format_model(insight::download_model("glm_2")), "logistic model")
+    testthat::expect_equal(format_model(insight::download_model("glm_3")), "logistic model")
+    testthat::expect_equal(format_model(insight::download_model("glm_4")), "probit model")
 
-  testthat::expect_equal(format_model(insight::download_model("lmerMod_1")), "linear mixed model")
-  testthat::expect_equal(format_model(insight::download_model("merMod_1")), "logistic mixed model")
-  testthat::expect_equal(format_model(insight::download_model("merMod_2")), "logistic mixed model")
+    testthat::expect_equal(format_model(insight::download_model("lmerMod_1")), "linear mixed model")
+    testthat::expect_equal(format_model(insight::download_model("merMod_1")), "logistic mixed model")
+    testthat::expect_equal(format_model(insight::download_model("merMod_2")), "logistic mixed model")
 
-  testthat::expect_equal(format_model(insight::download_model("stanreg_lm_1")), "Bayesian linear model")
-  testthat::expect_equal(format_model(insight::download_model("stanreg_glm_1")), "Bayesian logistic model")
+    testthat::expect_equal(format_model(insight::download_model("stanreg_lm_1")), "Bayesian linear model")
+    testthat::expect_equal(format_model(insight::download_model("stanreg_glm_1")), "Bayesian logistic model")
 
-  model <- glm(vs ~ mpg, data = mtcars, family = "poisson")
-  testthat::expect_equal(format_model(model), "general linear model (poisson family with a log link)")
-})
+    model <- glm(vs ~ mpg, data = mtcars, family = "poisson")
+    testthat::expect_equal(format_model(model), "general linear model (poisson family with a log link)")
+  })
+}
--- tests/testthat/test-report.bayesfactors.R
+++ tests/testthat/test-report.bayesfactors.R
@@ -1,20 +1,18 @@
-context("report.bayesfactor*")
+if (require("testthat") && require("bayestestR")) {
+  mod0 <- lm(Sepal.Length ~ 1, data = iris)
+  mod1 <- lm(Sepal.Length ~ Species, data = iris)
+  mod2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
+  mod3 <- lm(Sepal.Length ~ Species * Petal.Length, data = iris)
 
-library(bayestestR)
+  BFmodels <- bayestestR::bayesfactor_models(mod1, mod2, mod3, denominator = mod0)
 
-mod0 <- lm(Sepal.Length ~ 1, data = iris)
-mod1 <- lm(Sepal.Length ~ Species, data = iris)
-mod2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
-mod3 <- lm(Sepal.Length ~ Species * Petal.Length, data = iris)
+  testthat::test_that("models", {
+    testthat::expect_equal(nchar(text_short(report(BFmodels))), 186)
+  })
 
-BFmodels <- bayestestR::bayesfactor_models(mod1, mod2, mod3, denominator = mod0)
+  inc_bf <- bayestestR::bayesfactor_inclusion(BFmodels, prior_odds = c(1, 2, 3), match_models = TRUE)
 
-testthat::test_that("models", {
-  testthat::expect_equal(nchar(text_short(report(BFmodels))), 186)
-})
-
-inc_bf <- bayesfactor_inclusion(BFmodels, prior_odds = c(1, 2, 3), match_models = TRUE)
-
-testthat::test_that("inclusion", {
-  testthat::expect_equal(nchar(text_short(report(inc_bf))), 308)
-})
+  testthat::test_that("inclusion", {
+    testthat::expect_equal(nchar(text_short(report(inc_bf))), 308)
+  })
+}
