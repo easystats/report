bd1ad44a2183468c6b0d7b8bedef6e5b31eb6750
Daniel <mail@danielluedecke.de>
2020-12-11 15:52:27 +0100

    stylo

--- R/utils_data.R
+++ R/utils_data.R
@@ -58,18 +58,18 @@ data_rename <- function(data, pattern, replacement, safe = TRUE) {
 
 #' @rdname data_rename
 #' @export
-data_findcols <- function(data, pattern=NULL, starts_with=NULL, ends_with=NULL) {
+data_findcols <- function(data, pattern = NULL, starts_with = NULL, ends_with = NULL) {
   n <- names(data)
-  if(!is.null(pattern)){
+  if (!is.null(pattern)) {
     match <- c()
-    for(i in c(pattern)){
+    for (i in c(pattern)) {
       match <- c(match, n[grepl(i, n)])
     }
   }
-  if(!is.null(starts_with)){
+  if (!is.null(starts_with)) {
     match <- n[grepl(paste0(starts_with, ".*"), n)]
   }
-  if(!is.null(ends_with)){
+  if (!is.null(ends_with)) {
     match <- n[grepl(paste0(".*", ends_with), n)]
   }
   match
