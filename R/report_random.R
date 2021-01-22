46f822fa3239b7c9d0329fd890b72be99d68d052
mutlusun <mutlusun@users.noreply.github.com>
2021-01-22 09:20:02 +0100

    add initial support for brms models
    - models with multiple dependent variables are not supported yet

--- R/report_random.R
+++ R/report_random.R
@@ -27,6 +27,12 @@
 #'   r
 #'   summary(r)
 #' }
+#' if(require("brms")){
+#'   model <- brm(mpg ~ disp + (1 | cyl), data = mtcars, refresh=0, iter=1000)
+#'   r <- report_random(model)
+#'   r
+#'   summary(r)
+#' }
 #' @export
 report_random <- function(x, ...) {
   UseMethod("report_random")
