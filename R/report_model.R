856244387c2bb173674da4b4e5bcdcca70c126e1
Dominique Makowski <dom.mak19@gmail.com>
2021-04-12 22:37:01 +0800

    more

--- R/report_model.R
+++ R/report_model.R
@@ -23,6 +23,7 @@
 #' report_model(lm(Sepal.Length ~ Petal.Length * Species, data = iris))
 #' report_model(glm(vs ~ disp, data = mtcars, family = "binomial"))
 #'
+#' \donttest{
 #' # Mixed models
 #' if (require("lme4")) {
 #'   model <- lme4::lmer(Sepal.Length ~ Petal.Length + (1 | Species), data = iris)
@@ -33,6 +34,7 @@
 #' if (require("rstanarm")) {
 #'   model <- stan_glm(Sepal.Length ~ Species, data = iris, refresh = 0, iter = 600)
 #'   report_model(model)
+#' }
 #' }
 #' @export
 report_model <- function(x, table = NULL, ...) {
