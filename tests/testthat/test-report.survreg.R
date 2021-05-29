bb6e74e126055dece841d61c20487f6320495ec2
Indrajeet Patil <inderonline1988@gmail.com>
2021-05-29 08:39:04 +0200

    address Brenton's comment; test for survreg

---
+++ tests/testthat/test-report.survreg.R
@@ -0,0 +1,15 @@
+if (require("testthat") && require("report") && require("survival")) {
+  test_that("report-survreg", {
+
+    # model
+    set.seed(123)
+    mod_survreg <-
+      survival::survreg(
+        formula = Surv(futime, fustat) ~ ecog.ps + rx,
+        data = ovarian,
+        dist = "logistic"
+      )
+
+    expect_snapshot(report(mod_survreg))
+  })
+}
