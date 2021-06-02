8fcf8c139b3704e51b6650eb2d8010c8869566c7
Dominique Makowski <dom.mak19@gmail.com>
2021-06-02 22:42:16 +0800

    check fix

--- R/utils_data.R
+++ R/utils_data.R
@@ -96,7 +96,7 @@ data_remove <- function(data, pattern) {
 #' @rdname data_rename
 #' @export
 data_reorder <- function(data, cols, safe = TRUE) {
-  remaining_columns <- setdiff(colnames(data), cols)
+  remaining_columns <- setdiff(names(data), cols)
   if (isTRUE(safe)) cols <- cols[cols %in% names(data)]
   data[, c(cols, remaining_columns)]
 }
