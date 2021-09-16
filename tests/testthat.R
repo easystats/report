b0bc9591156e4f368fd72e029126968fc86adae4
Daniel <mail@danielluedecke.de>
2021-09-16 09:15:30 +0200

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,18 @@
 library(testthat)
 library(report)
 
-if (length(strsplit(packageDescription("report")$Version, "\\.")[[1]]) > 3) {
+is_dev_version <- length(strsplit(packageDescription("report")$Version, "\\.")[[1]]) > 3
+
+if (is_dev_version) {
   Sys.setenv("RunAllreportTests" = "yes")
 } else {
   Sys.setenv("RunAllreportTests" = "no")
 }
 
+si <- Sys.info()
+
 osx <- tryCatch(
   {
-    si <- Sys.info()
     if (!is.null(si["sysname"])) {
       si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
     } else {
@@ -21,4 +24,26 @@ osx <- tryCatch(
   }
 )
 
-test_check("report")
+solaris <- tryCatch(
+  {
+    if (!is.null(si["sysname"])) {
+      grepl("SunOS", si["sysname"], ignore.case = TRUE)
+    } else {
+      FALSE
+    }
+  },
+  error = function(e) {
+    FALSE
+  }
+)
+
+# disable / enable if needed
+if (.Platform$OS.type == "unix" && is_dev_version) {
+  Sys.setenv("RunAllreportStanTests" = "yes")
+} else {
+  Sys.setenv("RunAllreportStanTests" = "no")
+}
+
+if (!osx && !solaris) {
+  test_check("report")
+}
