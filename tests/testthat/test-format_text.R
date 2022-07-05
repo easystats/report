3fa21a20337364a3abebfbf6f9eea43124dc3531
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-07-05 07:36:29 +0200

    these functions live in datawizard now

--- tests/testthat/test-format_text.R
+++
@@ -1,6 +0,0 @@
-test_that("format_text", {
-  expect_equal(text_concatenate(c("A", "B", "C")), "A, B and C", tolerance = 0)
-
-  x <- paste(rep("a very long string", 50), collapse = " \n")
-  expect_equal(nchar(text_wrap(x, width = 25)), 999, tolerance = 0)
-})
