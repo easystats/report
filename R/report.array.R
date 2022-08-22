7640d22d6ad8d5be0fd45e62b4c37fa107ff8d92
Ryan Chitwood <26073857+11rchitwood@users.noreply.github.com>
2022-08-22 16:22:44 -0400

    Pass dots in report.matrix and report.array

--- R/report.array.R
+++ R/report.array.R
@@ -14,17 +14,7 @@
 #' summary(as.data.frame(r))
 #' @return An object of class [report()].
 #' @export
-report.array <- function(x,
-                          n = FALSE,
-                          centrality = "mean",
-                          dispersion = TRUE,
-                          range = TRUE,
-                          distribution = FALSE,
-                          levels_percentage = "auto",
-                          digits = 2,
-                          n_entries = 3,
-                          missing_percentage = "auto",
-                          ...) {
+report.array <- function(x, ...) {
   x <- as.data.frame(x)
-  report(x)
+  report(x, ...)
 }
