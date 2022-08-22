33dff8b5687d6989b71d33d458cd04639d034e2b
Ryan Chitwood <26073857+11rchitwood@users.noreply.github.com>
2022-08-22 14:53:58 -0400

    Implement and test report.matrix

---
+++ R/report.matrix.R
@@ -0,0 +1,30 @@
+#' Reporting Matrices
+#'
+#' Create reports for matrices.
+#'
+#' @inheritParams report.data.frame
+#'
+#' @examples
+#' library(report)
+#'
+#' r <- report(WorldPhones)
+#' r
+#' summary(r)
+#' as.data.frame(r)
+#' summary(as.data.frame(r))
+#' @return An object of class [report()].
+#' @export
+report.matrix <- function(x,
+                          n = FALSE,
+                          centrality = "mean",
+                          dispersion = TRUE,
+                          range = TRUE,
+                          distribution = FALSE,
+                          levels_percentage = "auto",
+                          digits = 2,
+                          n_entries = 3,
+                          missing_percentage = "auto",
+                          ...) {
+  x <- as.data.frame(x)
+  report(x)
+}
