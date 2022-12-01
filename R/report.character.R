easystats/report

commit ffd4b95ff4dd5bc07cb0ac10ee456fb237d4e5f0

remove redundant logical comparison lints

R/report.character.R
@@ -58,22 +58,18 @@ report_table.character <- function(x,
   table_full$percentage_Missing <- table_full$n_Missing / table_full$n_Obs * 100
   table <- table_full
 
-  if (missing_percentage == TRUE) {
+  if (missing_percentage) {
     table <- datawizard::data_remove(table, "n_Missing")
   } else {
     table <- datawizard::data_remove(table, "percentage_Missing")
   }
-
 
   as.report_table(table_full, summary = table, entries = n_char)
 }
 
 
 
-
 # report_parameters -------------------------------------------------------
-
-
 
 #' @export
 report_parameters.character <- function(x,
@@ -96,7 +92,7 @@ report_parameters.character <- function(x,
   }
   entries <- attributes(table)$entries
 
-  if (levels_percentage == TRUE) {
+  if (levels_percentage) {
     text <- paste0(entries$Entry, " (", insight::format_value(entries$percentage_Entry, as_percent = TRUE), ")")
   } else {
     text <- paste0(entries$Entry, " (n = ", entries$n_Entry, ")")
@@ -162,7 +158,7 @@ report_text.character <- function(x,
   # Missing
   text_n_Missing <- paste0(table$n_Missing[1], " missing")
   text_percentage_Missing <- paste0(insight::format_value(table$percentage_Missing[1]), "% missing")
-  if (missing_percentage == TRUE) {
+  if (missing_percentage) {
     text_full <- paste0(text_full, "(", text_percentage_Missing, ")")
     if (table$n_Missing[1] > 0) {
       text <- paste0(text, " (", text_percentage_Missing, ")")
@@ -203,7 +199,7 @@ report_statistics.character <- function(x,
   }
   entries <- attributes(table)$entries
 
-  if (levels_percentage == TRUE) {
+  if (levels_percentage) {
     text <- paste0(entries$Entry, ", ", insight::format_value(entries$percentage_Entry, as_percent = TRUE), "%")
   } else {
     text <- paste0(entries$Entry, ", n = ", entries$n_Entry)

R/report.numeric.R
@@ -87,13 +87,13 @@ report_table.numeric <- function(x,
   # Centrality and dispersion
   if (!isFALSE(centrality) && !is.null(centrality)) {
     if (centrality == "median") {
-      if (dispersion == TRUE) {
+      if (dispersion) {
         table <- datawizard::data_remove(table, c("Mean", "SD"))
       } else {
         table <- datawizard::data_remove(table, c("Mean", "SD", "MAD"))
       }
     } else {
-      if (dispersion == TRUE) {
+      if (dispersion) {
         table <- datawizard::data_remove(table, c("Median", "MAD"))
       } else {
         table <- datawizard::data_remove(table, c("Median", "MAD", "SD"))
@@ -102,18 +102,18 @@ report_table.numeric <- function(x,
   }
 
   # Range
-  if (range == FALSE) {
+  if (!range) {
     table <- datawizard::data_remove(table, c("Min", "Max"))
   }
 
   # Distribution
-  if (distribution == FALSE) {
+  if (!distribution) {
     table <- datawizard::data_remove(table, c("Skewness", "Kurtosis"))
   }
 
   # Missing
   if (!is.null(missing_percentage)) {
-    if (missing_percentage == TRUE) {
+    if (missing_percentage) {
       table <- datawizard::data_remove(table, "n_Missing")
       table_full <- datawizard::data_remove(table_full, "n_Missing")
     } else {
@@ -181,7 +181,7 @@ report_parameters.numeric <- function(x,
 
   # Missing
   if (!is.null(missing_percentage)) {
-    if (missing_percentage == TRUE) {
+    if (missing_percentage) {
       n_missing <- table$percentage_Missing[1]
       text_missing <- paste0(insight::format_value(table$percentage_Missing[1], protect_integers = TRUE, digits = digits), "% missing")
     } else {
@@ -214,12 +214,12 @@ report_parameters.numeric <- function(x,
   if (!isFALSE(centrality) && !is.null(centrality)) {
     if (centrality == "median") {
       text <- text[!names(text) %in% c("Mean", "Dispersion_Mean")]
-      if (dispersion == FALSE) {
+      if (!dispersion) {
         text <- text[!names(text) %in% c("Dispersion_Median")]
       }
     } else {
       text <- text[!names(text) %in% c("Median", "Dispersion_Median")]
-      if (dispersion == FALSE) {
+      if (!dispersion) {
         text <- text[!names(text) %in% c("Dispersion_Mean")]
       }
     }
@@ -228,12 +228,12 @@ report_parameters.numeric <- function(x,
   }
 
   # Range
-  if (range == FALSE) {
+  if (!range) {
     text <- text[!names(text) %in% c("Range")]
   }
 
   # Distribution
-  if (distribution == FALSE) {
+  if (!distribution) {
     text <- text[!names(text) %in% c("Skewness", "Kurtosis")]
   }
 

R/cite_easystats.R
@@ -339,7 +339,7 @@ print.cite_easystats <- function(x, what = "all", ...) {
     stop("`x` must be a logical vector.", call. = FALSE)
   }
   count <- sum(x)
-  x[x == FALSE] <- ""
+  x[!x] <- ""
   x[x != ""] <- letters[seq_len(count)]
   x
 }

R/report.factor.R
@@ -32,7 +32,7 @@ report_table.factor <- function(x, levels_percentage = "auto", ...) {
 
   # Shorten
   table <- table_full
-  if (levels_percentage == FALSE) {
+  if (!levels_percentage) {
     table <- datawizard::data_remove(table, "percentage_Obs")
   }
 

R/report_effectsize.R
@@ -119,21 +119,21 @@ print.report_effectsize <- function(x, ...) {
   two_sd <- attributes(x)$two_sd
 
   if (method == "refit") {
-    if (robust == TRUE) {
+    if (robust) {
       text <- "(using the median and the MAD, a robust equivalent of the SD) "
     } else {
       text <- ""
     }
     text <- paste0("Standardized parameters were obtained by fitting the model on a standardized version ", text, "of the dataset.")
   } else if (method == "2sd") {
-    if (robust == TRUE) {
+    if (robust) {
       text <- "MAD (a median-based equivalent of the SD) "
     } else {
       text <- "SD "
     }
     text <- paste0("Standardized parameters were obtained by standardizing the data by 2 times the ", text, " (see Gelman, 2008).")
   } else if (method %in% c("smart", "basic", "posthoc")) {
-    if (robust == TRUE) {
+    if (robust) {
       text <- "median and the MAD (a median-based equivalent of the SD) of the response variable."
     } else {
       text <- "mean and the SD of the response variable."

R/report_parameters.R
@@ -210,7 +210,7 @@ print.report_parameters <- function(x, ...) {
     text <- ""
   }
 
-  if (only_when_insufficient == FALSE) {
+  if (!only_when_insufficient) {
     text
   } else {
     ifelse(convergence != "converged" | stability != "sufficient", text, "")

man/cite_easystats.Rd
@@ -50,7 +50,6 @@ A list of class \code{"cite_easystats"} with elements:
 A convenient function for those who wish to cite the easystats packages.
 }
 \examples{
-
 \dontrun{
 # Cite just the 'easystats' umbrella package:
 cite_easystats()

commit bbeb4ab4c7ee829b4a30bced8561016b66fbb38e

fix regression
introduced in https://github.com/easystats/report/commit/ffd4b95ff4dd5bc07cb0ac10ee456fb237d4e5f0

R/report.character.R
@@ -58,7 +58,7 @@ report_table.character <- function(x,
   table_full$percentage_Missing <- table_full$n_Missing / table_full$n_Obs * 100
   table <- table_full
 
-  if (missing_percentage) {
+  if (isTRUE(missing_percentage)) {
     table <- datawizard::data_remove(table, "n_Missing")
   } else {
     table <- datawizard::data_remove(table, "percentage_Missing")
@@ -158,7 +158,7 @@ report_text.character <- function(x,
   # Missing
   text_n_Missing <- paste0(table$n_Missing[1], " missing")
   text_percentage_Missing <- paste0(insight::format_value(table$percentage_Missing[1]), "% missing")
-  if (missing_percentage) {
+  if (isTRUE(missing_percentage)) {
     text_full <- paste0(text_full, "(", text_percentage_Missing, ")")
     if (table$n_Missing[1] > 0) {
       text <- paste0(text, " (", text_percentage_Missing, ")")

R/report.numeric.R
@@ -113,7 +113,7 @@ report_table.numeric <- function(x,
 
   # Missing
   if (!is.null(missing_percentage)) {
-    if (missing_percentage) {
+    if (isTRUE(missing_percentage)) {
       table <- datawizard::data_remove(table, "n_Missing")
       table_full <- datawizard::data_remove(table_full, "n_Missing")
     } else {
@@ -181,7 +181,7 @@ report_parameters.numeric <- function(x,
 
   # Missing
   if (!is.null(missing_percentage)) {
-    if (missing_percentage) {
+    if (isTRUE(missing_percentage)) {
       n_missing <- table$percentage_Missing[1]
       text_missing <- paste0(insight::format_value(table$percentage_Missing[1], protect_integers = TRUE, digits = digits), "% missing")
     } else {