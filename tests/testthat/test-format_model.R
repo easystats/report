d6cb2a9a87dfe90e4904c2dcc546d86a2e8de208
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-12-25 17:48:18 +0100

    fix noSuggests workflow

--- tests/testthat/test-format_model.R
+++ tests/testthat/test-format_model.R
@@ -1,13 +1,15 @@
-if (requiet("testthat")) {
-  test_that("format_model", {
-    expect_equal(format_model(lm(Sepal.Length ~ Petal.Length * Species, data = iris)), "linear model")
-    expect_equal(format_model(glm(vs ~ disp, data = mtcars, family = "binomial")), "logistic model")
-    expect_equal(format_model(glm(vs ~ disp, data = mtcars, family = binomial(link = "probit"))), "probit model")
-    expect_equal(format_model(glm(vs ~ mpg, data = mtcars, family = "poisson")), "poisson model")
-  })
-}
 
 test_that("format_model", {
+  expect_equal(format_model(lm(Sepal.Length ~ Petal.Length * Species, data = iris)), "linear model")
+  expect_equal(format_model(glm(vs ~ disp, data = mtcars, family = "binomial")), "logistic model")
+  expect_equal(format_model(glm(vs ~ disp, data = mtcars, family = binomial(link = "probit"))), "probit model")
+  expect_equal(format_model(glm(vs ~ mpg, data = mtcars, family = "poisson")), "poisson model")
+})
+
+
+test_that("format_model", {
+  requiet("lme4")
+
   expect_equal(format_model(lme4::lmer(wt ~ cyl + (1 | gear), data = mtcars)), "linear mixed model")
   expect_equal(format_model(lme4::glmer(vs ~ cyl + (1 | gear), data = mtcars, family = "binomial")), "logistic mixed model")
   expect_equal(format_model(lme4::glmer(vs ~ drat + cyl + (1 | gear), data = mtcars, family = "binomial")), "logistic mixed model")
@@ -15,9 +17,9 @@ test_that("format_model", {
 
 
 
-if (requiet("rstanarm")) {
-  test_that("format_model", {
-    expect_equal(format_model(suppressWarnings(rstanarm::stan_glm(mpg ~ wt, data = mtcars, refresh = 0, iter = 50))), "Bayesian linear model")
-    expect_equal(format_model(suppressWarnings(rstanarm::stan_glm(vs ~ wt, data = mtcars, family = "binomial", refresh = 0, iter = 50))), "Bayesian logistic model")
-  })
-}
+
+test_that("format_model", {
+  requiet("rstanarm")
+  expect_equal(format_model(suppressWarnings(rstanarm::stan_glm(mpg ~ wt, data = mtcars, refresh = 0, iter = 50))), "Bayesian linear model")
+  expect_equal(format_model(suppressWarnings(rstanarm::stan_glm(vs ~ wt, data = mtcars, family = "binomial", refresh = 0, iter = 50))), "Bayesian logistic model")
+})
