b1b606ba6e456bffd18f5167996b35dceb1b0574
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2023-05-14 10:39:40 -0400

    remove cohen d

--- R/report.estimate_contrasts.R
+++ R/report.estimate_contrasts.R
@@ -5,8 +5,6 @@
 #' @param x Object of class `estimate_contrasts`.
 #' @param table Provide the output of  `report_table()` to avoid its
 #'   re-computation.
-#' @param effectsize Provide the output of `report_effectsize()` to avoid
-#'   its re-computation.
 #' @inheritParams report
 #'
 #' @inherit report return seealso
@@ -25,46 +23,12 @@ report.estimate_contrasts <- function(x, ...) {
   as.report(text, table = table, ...)
 }
 
-
-# report_effectsize -------------------------------------------------------
-
-#' @rdname report.estimate_contrasts
-#' @export
-report_effectsize.estimate_contrasts <- function(x, ...) {
-  att <- attributes(x)
-  dat <- insight::get_data(att$model)
-
-  dat_wide <- datawizard::data_to_wide(
-    dat,
-    values_from = "Sepal.Width",
-    names_from = "Species"
-  )
-
-  eff <- lapply(seq_len(nrow(x)), function(i) {
-    effectsize::cohens_d(x = x$Level1[i], y = x$Level2[i], data = dat_wide)
-  })
-
-  eff <- do.call(rbind, eff)
-
-  names(eff)[-1] <- paste0("Cohens_d_", names(eff)[-1])
-
-  eff
-}
-
-
 # report_table ------------------------------------------------------------
 
-
 #' @rdname report.estimate_contrasts
 #' @export
-report_table.estimate_contrasts <- function(x, effectsize = NULL, ...) {
-  if (is.null(effectsize)) {
-    effectsize <- report_effectsize(x)
-  }
-  out <- as.report_table(cbind(x, effectsize))
-
-  # Return output
-  out
+report_table.estimate_contrasts <- function(x, ...) {
+  as.report_table(x)
 }
 
 # report_text ------------------------------------------------------------
@@ -75,13 +39,10 @@ report_text.estimate_contrasts <- function(x, table = NULL, ...) {
   f_table <- insight::format_table(table)
 
   text <- paste0("The difference between ", x$Level1, " and ", x$Level2, " is ",
-    ifelse(x$Difference < 0, " negative,", "positive,"), " statistically ",
-    ifelse(x$p < 0.05, "significant,", "non-significant,"), " and ",
-    effectsize::interpret_cohens_d(table$Cohens_d), " ",
-    "(difference = ", f_table$Difference, ", 95% CI ", f_table$CI, ", ",
-    names(f_table)[6], " = ", f_table[[6]], ", ", insight::format_p(table$p),
-    ", ", "Cohen's d = ", f_table$`Cohen's d`, ", 95% CI ", f_table$`Cohens_d 95% CI`,
-    ")",
+    ifelse(x$Difference < 0, " negative", "positive"), " and statistically ",
+    ifelse(x$p < 0.05, "significant", "non-significant"),
+    " (difference = ", f_table$Difference, ", 95% CI ", f_table$`95% CI`, ", ",
+    names(f_table)[6], " = ", f_table[[6]], ", ", insight::format_p(table$p), ")",
     collapse = ". "
   )
 
