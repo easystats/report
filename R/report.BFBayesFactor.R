59f3ee8e0a16fef0a9011bb1c7cb154c56e430d3
Dominique Makowski <dom.mak19@gmail.com>
2024-03-28 13:31:47 +0000

    add

---
+++ R/report.BFBayesFactor.R
@@ -0,0 +1,62 @@
+#' Reporting `BFBayesFactor` objects from the `BayesFactor` package
+#'
+#' Interpretation of the Bayes factor output from the `BayesFactor` package.
+#'
+#' @param x An object of class `BFBayesFactor`.
+#' @param h0,h1 Names of the null and alternative hypotheses.
+#'
+#' @examplesIf requireNamespace("BayesFactor", quietly = TRUE)
+#' \donttest{
+#' library(BayesFactor)
+#'
+#' rez <- BayesFactor::ttestBF(iris$Sepal.Width, iris$Sepal.Length)
+#' report_statistics(rez)
+#' report(rez, h0="the null hypothesis", h1="the alternative")
+#'
+#' rez <- BayesFactor::correlationBF(iris$Sepal.Width, iris$Sepal.Length)
+#' report(rez)
+#' }
+#'
+#' @export
+report.BFBayesFactor <- function(x, h0="H0", h1="H1", ...) {
+  param <- parameters::parameters(x, ...)
+  bf <- param$BF
+  dir <- ifelse(bf < 1, "h0", "h1")
+
+
+  if (dir == "h1") {
+    text <- paste0("There is ",
+                  effectsize::interpret_bf(bf, ...),
+                  " ",
+                  h1,
+                  " over ",
+                  h0,
+                  " (", report_statistics(x), ").")
+  } else {
+    text <- paste0("There is ",
+                  effectsize::interpret_bf(1/bf, ...),
+                  " ",
+                  h0,
+                  " over ",
+                  h1,
+                  " (", report_statistics(x), ").")
+  }
+  text
+}
+
+
+
+#' @rdname report.BFBayesFactor
+#' @export
+report_statistics.BFBayesFactor <- function(x, table = NULL, ...) {
+  if(is.null(table)) {
+    table <- parameters::parameters(x, ...)
+  }
+
+  bf <- table$BF
+  text <- ifelse(bf < 1,
+                 insight::format_bf(1/bf, name="BF01"),
+                 insight::format_bf(bf, name="BF10"))
+  text
+}
+
