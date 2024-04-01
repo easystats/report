d21553c7685940acbc67c1d6e6615d851afdce3a
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2024-04-01 12:27:37 +0200

    fix lints, build_reference_index

--- R/report.BFBayesFactor.R
+++ R/report.BFBayesFactor.R
@@ -12,48 +12,54 @@
 #' library(BayesFactor)
 #'
 #' rez <- BayesFactor::ttestBF(iris$Sepal.Width, iris$Sepal.Length)
-#' report_statistics(rez, exact=TRUE)  # Print exact BF
-#' report(rez, h0="the null hypothesis", h1="the alternative")
+#' report_statistics(rez, exact = TRUE) # Print exact BF
+#' report(rez, h0 = "the null hypothesis", h1 = "the alternative")
 #'
 #' rez <- BayesFactor::correlationBF(iris$Sepal.Width, iris$Sepal.Length)
 #' report(rez)
 #' }
 #'
 #' @export
-report.BFBayesFactor <- function(x, h0="H0", h1="H1", ...) {
-  if ("BFlinearModel" %in% class(x@numerator[[1]])) {
+report.BFBayesFactor <- function(x, h0 = "H0", h1 = "H1", ...) {
+  if (inherits("BFlinearModel", class(x@numerator[[1]]))) {
     return(report(bayestestR::bayesfactor_models(x), ...))
   }
 
   if (length(x@numerator) > 1) {
-    insight::format_alert("Multiple `BFBayesFactor` models detected - reporting for the first numerator model.",
-                          "See help(\"get_parameters\", package = \"insight\").")
+    insight::format_alert(
+      "Multiple `BFBayesFactor` models detected - reporting for the first numerator model.",
+      "See help(\"get_parameters\", package = \"insight\")."
+    )
     x <- x[1]
   }
 
   param <- parameters::parameters(x[1], ...)
   bf <- param$BF
-  dir <- ifelse(bf < 1, "h0", "h1")
+  other_dir <- ifelse(bf < 1, "h0", "h1")
 
 
-  if (dir == "h1") {
-    text <- paste0("There is ",
-                  effectsize::interpret_bf(bf, ...),
-                  " ",
-                  h1,
-                  " over ",
-                  h0,
-                  " (", report_statistics(x, ...), ").")
+  if (other_dir == "h1") {
+    other_text <- paste0(
+      "There is ",
+      effectsize::interpret_bf(bf, ...),
+      " ",
+      h1,
+      " over ",
+      h0,
+      " (", report_statistics(x, ...), ")."
+    )
   } else {
-    text <- paste0("There is ",
-                  effectsize::interpret_bf(1/bf, ...),
-                  " ",
-                  h0,
-                  " over ",
-                  h1,
-                  " (", report_statistics(x, ...), ").")
+    other_text <- paste0(
+      "There is ",
+      effectsize::interpret_bf(1 / bf, ...),
+      " ",
+      h0,
+      " over ",
+      h1,
+      " (", report_statistics(x, ...), ")."
+    )
   }
-  text
+  other_text
 }
 
 
@@ -61,19 +67,21 @@ report.BFBayesFactor <- function(x, h0="H0", h1="H1", ...) {
 #' @rdname report.BFBayesFactor
 #' @export
 report_statistics.BFBayesFactor <- function(x, table = NULL, ...) {
-  if(is.null(table)) {
+  if (is.null(table)) {
     if (length(x@numerator) > 1) {
-      insight::format_alert("Multiple `BFBayesFactor` models detected - reporting for the first numerator model.",
-                            "See help(\"get_parameters\", package = \"insight\").")
+      insight::format_alert(
+        "Multiple `BFBayesFactor` models detected - reporting for the first numerator model.",
+        "See help(\"get_parameters\", package = \"insight\")."
+      )
       x <- x[1]
     }
     table <- parameters::parameters(x, ...)
   }
 
   bf <- table$BF
-  text <- ifelse(bf < 1,
-                 insight::format_bf(1/bf, name="BF01", ...),
-                 insight::format_bf(bf, name="BF10", ...))
-  text
+  other_text <- ifelse(bf < 1,
+    insight::format_bf(1 / bf, name = "BF01", ...),
+    insight::format_bf(bf, name = "BF10", ...)
+  )
+  other_text
 }
-
