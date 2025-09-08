5be0efe795b482db7a1898cec2a5eeb3a0176d90
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2025-09-07 22:31:17 -0400

    add report.Rcheck to gitignore

--- report.Rcheck/00_pkg_src/report/R/zzz.R
+++
@@ -1,3 +0,0 @@
-.support_unicode <- l10n_info()$`UTF-8` |
-  isTRUE(.Options$cli.unicode) |
-  nzchar(Sys.getenv("RSTUDIO_USER_IDENTITY"))
--- report.Rcheck/tests/testthat/test-report.numeric.R
+++
@@ -1,5 +0,0 @@
-test_that("Median removes NA", {
-  x <- c(1, 2, NA, 4)
-  result <- report_table(x)$Median
-  expect_equal(result, median(x, na.rm = TRUE))
-})
